053b1b4fd585090d29c977007b0f1c91ee715cd5
DESKTOP-30CC3QK\moham <Melasser@ejada.com>
2020-08-24 14:30:41 +0200

    update in unit test

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,5 +1,8 @@
 using AzureDevOpsCITest;
+using AzureDevOpsCITest.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Xunit;
 
 namespace UnitTestProject1
 {
@@ -10,11 +13,42 @@ namespace UnitTestProject1
         public void TestMethod1()
         {
             int x = 10;
-            int y = 15;
+            int y = 5;
             //int z = x + y;
             CalcService calcService = new CalcService();
             int result = calcService.Add(x, y);
             Assert.AreEqual(15, result);
         }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int x = 10;
+            int y = 5;
+            CalculatorController calculatorController = new CalculatorController();
+            var result = calculatorController.Add(x, y);
+            Assert.AreEqual(15, result);
+        }
+
+       
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            try
+            {
+                int x = 10;
+                int y = 5;
+                CalculatorController calculatorController = new CalculatorController();
+                var result = calculatorController.Add(x, y);
+                Assert.AreEqual(15, result);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Expected no exception, but got: " + ex.Message);
+            }
+        }
+
+       
     }
 }
